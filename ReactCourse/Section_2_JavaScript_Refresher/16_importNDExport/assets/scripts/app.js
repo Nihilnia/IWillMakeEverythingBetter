c0fe63cd81a2e6a61e116f359116ef01f5eed176
Nihil <35506793+Nihilnia@users.noreply.github.com>
2025-02-01 22:06:14 +0300

    Nihil is started.

---
+++ ReactCourse/Section_2_JavaScript_Refresher/16_importNDExport/assets/scripts/app.js
@@ -0,0 +1,14 @@
+//Way 1:
+//import { apiKey } from "./util.js";
+
+//Way 2:
+// import apiKey from "./util.js";
+
+// document.querySelector("body").innerHTML += `<h2>${apiKey}</h2>`;
+
+//Way 3:
+import * as util from "./util.js";
+
+console.log(util.default);
+console.log(util.Gloria);
+console.log(util.Alt);
---
+++ ReactCourse/Section_2_JavaScript_Refresher/16_importNDExport/assets/scripts/util.js
@@ -0,0 +1,11 @@
+//Way 1:
+// export let apiKey = "4957696c6c4d616b6545766572797468696e67426574746572";
+
+//Way 2:
+// export default "4957696c6c4d616b6545766572797468696e67426574746572";
+
+//Way 3:
+
+export default "4957696c6c4d616b6545766572797468696e67426574746572";
+export let Gloria = "Project";
+export let Alt = "Rogue AI";

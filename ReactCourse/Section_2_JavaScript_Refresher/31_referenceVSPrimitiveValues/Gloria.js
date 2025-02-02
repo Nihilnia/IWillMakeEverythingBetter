29e91cbd3bd4ed3fea71adcbd6d6fbbb662ed995
Nihil <35506793+Nihilnia@users.noreply.github.com>
2025-02-02 18:42:18 +0300

    Progress.

---
+++ ReactCourse/Section_2_JavaScript_Refresher/31_referenceVSPrimitiveValues/Gloria.js
@@ -0,0 +1,9 @@
+/*
+!Basically, 
+when you define a variable your machine hold a value in the RAM for it. 
+But when you want to change that variable that RAM value (address lets say) is changing. 
+Because of being primitive types.
+
+But its not the case for reference objects (objects, arrays, functions)
+When you make a change javaScript finds that address in the RAM and changes the values of, not the address.
+*/

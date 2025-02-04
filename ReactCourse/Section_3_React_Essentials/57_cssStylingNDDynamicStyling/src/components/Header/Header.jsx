0204d8ee4e7cc641e06d4e881b7ba7b6bdee6f95
Nihil <35506793+Nihilnia@users.noreply.github.com>
2025-02-04 15:53:33 +0300

    Progress

---
+++ ReactCourse/Section_3_React_Essentials/57_cssStylingNDDynamicStyling/src/components/Header/Header.jsx
@@ -0,0 +1,26 @@
+import "./Header.css";
+
+import reactImg from "../../assets/arkadiy-demchenko-stills.jpg";
+
+const reactDescriptions = ["Fundamental", "Crucial", "Core"];
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
+function Header() {
+  const description = reactDescriptions[getRandomInt(2)];
+
+  return (
+    <header>
+      <img src={reactImg} alt="Stylized atom" />
+      <h1>Run Nihil</h1>
+      <p>
+        {description} React concepts you will need for almost any app you are
+        going to build!
+      </p>
+    </header>
+  );
+}
+
+export default Header;

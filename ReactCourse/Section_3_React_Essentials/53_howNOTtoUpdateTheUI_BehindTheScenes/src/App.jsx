02c38d0a9fc683377411ddf3ac6a561c26d11967
Nihil <35506793+Nihilnia@users.noreply.github.com>
2025-02-04 13:09:17 +0300

    Let' s go to the state and hooks, excited

---
+++ ReactCourse/Section_3_React_Essentials/53_howNOTtoUpdateTheUI_BehindTheScenes/src/App.jsx
@@ -0,0 +1,49 @@
+import Header from "./components/Header/Header.jsx";
+import CoreConcept from "./components/CoreConcept.jsx";
+
+import { CORE_CONCEPTS } from "./data.js";
+import TabButton from "./components/TabButton.jsx";
+
+function App() {
+  console.log("App component rendered.");
+
+  function handleSelect(selectedButton) {
+    console.log(selectedButton);
+  }
+
+  return (
+    <div>
+      <Header />
+      <main>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept
+              image={CORE_CONCEPTS[0].image}
+              title={CORE_CONCEPTS[0].title}
+              description={CORE_CONCEPTS[0].description}
+            />
+            {/* Shorter way */}
+            <CoreConcept {...CORE_CONCEPTS[1]} />
+            <CoreConcept {...CORE_CONCEPTS[2]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} />
+          </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton onSelect={() => handleSelect("components")}>
+              Components
+            </TabButton>
+            <TabButton onSelect={() => handleSelect("jsx")}>JSX</TabButton>
+            <TabButton onSelect={() => handleSelect("props")}>Props</TabButton>
+            <TabButton onSelect={() => handleSelect("state")}>State</TabButton>
+          </menu>
+          Dynamic Content
+        </section>
+      </main>
+    </div>
+  );
+}
+
+export default App;

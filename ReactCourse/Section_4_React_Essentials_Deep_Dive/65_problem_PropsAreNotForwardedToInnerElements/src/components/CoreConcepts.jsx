4ee0661ba82e45ac97ccce226db0a9830731ccc4
Nihil <35506793+Nihilnia@users.noreply.github.com>
2025-02-07 14:55:23 +0300

    Main theme - piano version

---
+++ ReactCourse/Section_4_React_Essentials_Deep_Dive/65_problem_PropsAreNotForwardedToInnerElements/src/components/CoreConcepts.jsx
@@ -0,0 +1,18 @@
+import CoreConcept from "./CoreConcept.jsx";
+import { CORE_CONCEPTS } from "../data.js";
+
+import Section from "./Section.jsx";
+
+function CoreConcepts() {
+  return (
+    <Section sectionID="core-concepts" title="Core Concepts">
+      <ul>
+        {CORE_CONCEPTS.map((f) => (
+          <CoreConcept key={f.title} {...f} />
+        ))}
+      </ul>
+    </Section>
+  );
+}
+
+export default CoreConcepts;

6efe3b9544b5e57bcd1faad55257419ee285bbc3
Nihil <35506793+Nihilnia@users.noreply.github.com>
2025-02-11 02:07:46 +0300

    Bla bla bla

---
+++ ReactCourse/Section_4_React_Essentials_Deep_Dive/75_conceptRepetition_WorkingWithState/src/components/Player.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+
+export default function Player({ name, symbol }) {
+  const [isEditing, setIsEditing] = useState(false);
+
+  function handleEditClick() {
+    setIsEditing(true);
+  }
+
+  const playerName = isEditing ? (
+    <input />
+  ) : (
+    <span className="player-name">{name}</span>
+  );
+
+  return (
+    <li>
+      <span className="player">
+        {playerName}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleEditClick}>Edit</button>
+    </li>
+  );
+}

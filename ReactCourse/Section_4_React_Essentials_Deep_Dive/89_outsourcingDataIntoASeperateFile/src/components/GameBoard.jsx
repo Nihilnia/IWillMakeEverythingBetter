a0615b434daa9de8af3620cc561b31530529e003
Nihil <35506793+Nihilnia@users.noreply.github.com>
2025-02-13 02:36:05 +0300

    winning combinations

---
+++ ReactCourse/Section_4_React_Essentials_Deep_Dive/89_outsourcingDataIntoASeperateFile/src/components/GameBoard.jsx
@@ -0,0 +1,38 @@
+const initialGameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+];
+
+export default function GameBoard({ onSelected, turns }) {
+  let gameBoard = initialGameBoard;
+
+  for (const turn of turns) {
+    const { square, player } = turn;
+    const { row, col } = square;
+    gameBoard[row][col] = player;
+  }
+
+  let theBoard = (
+    <ol id="game-board">
+      {gameBoard.map((row, rowIndex) => (
+        <li key={rowIndex}>
+          <ol>
+            {row.map((col, colIndex) => (
+              <li key={colIndex}>
+                <button
+                  onClick={() => onSelected(rowIndex, colIndex)}
+                  disabled={col !== null}
+                >
+                  {col}
+                </button>
+              </li>
+            ))}
+          </ol>
+        </li>
+      ))}
+    </ol>
+  );
+
+  return theBoard;
+}

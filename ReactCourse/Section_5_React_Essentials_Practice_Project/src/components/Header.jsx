40d796ffe08c2b3becfd3a9f2ea41a91ce8691f9
Nihil <35506793+Nihilnia@users.noreply.github.com>
2025-02-17 06:27:02 +0300

    Started to watch his way. Header added.

---
+++ ReactCourse/Section_5_React_Essentials_Practice_Project/src/components/Header.jsx
@@ -0,0 +1,10 @@
+import headerImg from "../assets/investment-calculator-logo.png";
+
+export default function Header() {
+  return (
+    <header id="header">
+      <img src={headerImg} alt="Image.." />
+      <h1>Investment calculatir</h1>
+    </header>
+  );
+}

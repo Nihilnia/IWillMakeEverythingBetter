ce13198c946e3fc0cb1c88dce868f67ece92c066
Nihil <35506793+Nihilnia@users.noreply.github.com>
2025-02-18 07:42:06 +0300

    Style attribute in jsx expect an object.

---
+++ ReactCourse/Section_6_Styling_React_Components/109_stylingReactAppsWInlineStyles/src/components/Gloria.jsx
@@ -0,0 +1,23 @@
+import "./Gloria.css";
+
+function Gloria() {
+  return (
+    <section id="Gloria">
+      <p
+        style={{
+          color: "rgba(0, 0, 0, 0.44)",
+          border: "1px solid rgba(0, 0, 0, 0.44)",
+          borderRadius: "10px",
+          padding: "10px",
+        }}
+      >
+        The Grid. A digital frontier. I tried to picture clusters of information
+        as they moved through the computer. What did they look like? Ships?
+        motorcycles? Were the circuits like freeways? I kept dreaming of a world
+        I thought I'd never see. And then, one day, I got in.
+      </p>
+    </section>
+  );
+}
+
+export default Gloria;

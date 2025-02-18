79c7c8850771443f4cd3a300aabae5b48d01faaa
Nihil <35506793+Nihilnia@users.noreply.github.com>
2025-02-18 05:27:09 +0300

    Project is done except when we enter negative values or zero to duration our app crashes. Time to fix that.

--- ReactCourse/Section_5_React_Essentials_Practice_Project/src/components/Results.jsx
+++ ReactCourse/Section_5_React_Essentials_Practice_Project/src/components/Results.jsx
@@ -1,9 +1,42 @@
-import { calculateInvestmentResults } from "../util/investment";
+import { calculateInvestmentResults, formatter } from "../util/investment";
 
 export default function Results({ res }) {
   const resultsData = calculateInvestmentResults(res);
-  console.log("resultsData");
-  console.log(resultsData);
 
-  return <p>askjdhkajsd</p>;
+  const initialInvestment =
+    resultsData[0].valueEndOfYear -
+    resultsData[0].interest -
+    resultsData[0].annualInvestment;
+
+  return (
+    <table id="result">
+      <thead>
+        <tr>
+          <th>Year</th>
+          <th>Investment Value</th>
+          <th>Interest (Year)</th>
+          <th>Total Interest</th>
+          <th>Invested Capital</th>
+        </tr>
+      </thead>
+      <tbody>
+        {resultsData.map((f) => {
+          const totalInterest =
+            f.valueEndOfYear - f.annualInvestment * f.year - initialInvestment;
+
+          const totalAmountInvested = f.valueEndOfYear - totalInterest;
+
+          return (
+            <tr key={f.year}>
+              <td>{f.year}</td>
+              <td>{formatter.format(f.valueEndOfYear)}</td>
+              <td>{formatter.format(f.interest)}</td>
+              <td>{formatter.format(totalInterest)}</td>
+              <td>{formatter.format(totalAmountInvested)}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
 }

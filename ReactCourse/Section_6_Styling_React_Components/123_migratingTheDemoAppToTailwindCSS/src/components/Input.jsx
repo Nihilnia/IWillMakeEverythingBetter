d614b5fbeb1e97aef61eb5aac5826e00e8381537
Nihil <35506793+Nihilnia@users.noreply.github.com>
2025-02-20 08:14:42 +0300

    Getting used with it.
    And ngl I like when I can guess the shorthands with tailwind. flex flex-row etc.

---
+++ ReactCourse/Section_6_Styling_React_Components/123_migratingTheDemoAppToTailwindCSS/src/components/Input.jsx
@@ -0,0 +1,19 @@
+export default function Input({ labelText, deneme, ...props }) {
+  let labelClasses = "block mb-2 text-xs font-bold tracking-wide uppercase";
+  let inputClasses = "w-full px-3 py-2 leading-tight border rounded shadow";
+
+  if (deneme) {
+    labelClasses += " text-red-400";
+    inputClasses += " text-red-500 bg-red-100 border-red-300";
+  } else {
+    labelClasses += " text-stone-300";
+    inputClasses += " text-gray-700 bg-stone-300";
+  }
+
+  return (
+    <p>
+      <label className={labelClasses}>{labelText}</label>
+      <input className={inputClasses} {...props} />
+    </p>
+  );
+}

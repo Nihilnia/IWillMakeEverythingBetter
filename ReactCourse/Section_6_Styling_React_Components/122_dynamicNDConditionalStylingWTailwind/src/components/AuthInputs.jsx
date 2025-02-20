4d97b7115b802449538ee153c8cd0b2ef25ed163
Nihil <35506793+Nihilnia@users.noreply.github.com>
2025-02-20 08:03:17 +0300

    Okaaaaayt

---
+++ ReactCourse/Section_6_Styling_React_Components/122_dynamicNDConditionalStylingWTailwind/src/components/AuthInputs.jsx
@@ -0,0 +1,67 @@
+import { useState } from "react";
+import { styled } from "styled-components";
+
+import TheButton from "./Button";
+import CustomInput from "./Input";
+
+const ControlContrainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+  margin-bottom: 1.5rem;
+`;
+
+const TextButton = styled.button`
+  color: #f0b322;
+  border: none;
+
+  &:hover {
+    color: #f0920e;
+  }
+`;
+
+export default function AuthInputs() {
+  const [enteredEmail, setEnteredEmail] = useState("");
+  const [enteredPassword, setEnteredPassword] = useState("");
+  const [submitted, setSubmitted] = useState(false);
+
+  function handleInputChange(identifier, value) {
+    if (identifier === "email") {
+      setEnteredEmail(value);
+    } else {
+      setEnteredPassword(value);
+    }
+  }
+
+  function handleLogin() {
+    setSubmitted(true);
+  }
+
+  const emailNotValid = submitted && !enteredEmail.includes("@");
+  const passwordNotValid = submitted && enteredPassword.trim().length < 6;
+
+  return (
+    <div id="auth-inputs">
+      <ControlContrainer>
+        <CustomInput
+          labelText="Email"
+          deneme={emailNotValid}
+          type="email"
+          onChange={(event) => handleInputChange("email", event.target.value)}
+        />
+        <CustomInput
+          labelText="Password"
+          deneme={passwordNotValid}
+          type="password"
+          onChange={(event) =>
+            handleInputChange("password", event.target.value)
+          }
+        />
+      </ControlContrainer>
+      <div className="actions">
+        <button type="button">Create a new account</button>
+        <TheButton onClick={handleLogin}>Sign In</TheButton>
+      </div>
+    </div>
+  );
+}

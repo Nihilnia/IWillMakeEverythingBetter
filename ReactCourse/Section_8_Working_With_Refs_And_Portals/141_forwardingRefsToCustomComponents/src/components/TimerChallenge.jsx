7c6f18954952a7a9d6333dbc0163c7859c884809
Nihil <35506793+Nihilnia@users.noreply.github.com>
2025-02-23 18:33:09 +0300

    Beautiful.

---
+++ ReactCourse/Section_8_Working_With_Refs_And_Portals/141_forwardingRefsToCustomComponents/src/components/TimerChallenge.jsx
@@ -0,0 +1,50 @@
+import { useRef, useState } from "react";
+import ResultModal from "./ResultModal";
+
+export default function TimerChallenge({ title, targetTime }) {
+  const [timerStarted, setTimerStarted] = useState(false);
+  const [timerExpired, setTimerExpired] = useState(false);
+
+  const timer = useRef();
+  const dialog = useRef();
+
+  function handleStart() {
+    setTimerStarted(true);
+    setTimerExpired(false);
+
+    timer.current = setTimeout(() => {
+      setTimerExpired(true);
+      setTimerStarted(false);
+      dialog.current.showModal();
+    }, targetTime * 1000);
+  }
+
+  function handleStop() {
+    setTimerStarted(false);
+    setTimerExpired(false);
+
+    console.log(timer);
+
+    clearTimeout(timer.current);
+  }
+
+  return (
+    <>
+      <ResultModal ref={dialog} result="lost" targetTime={targetTime} />
+      <section className="challenge">
+        <h2>{title}</h2>
+        <p className="challenge-time">
+          {targetTime} second{targetTime > 1 ? "s" : ""}
+        </p>
+        <p>
+          <button onClick={timerStarted ? handleStop : handleStart}>
+            {timerStarted ? "Stop" : "Start"} Challenge
+          </button>
+        </p>
+        <p className={timerStarted ? "active" : undefined}>
+          {timerStarted ? "Time is running.." : "Not active"}
+        </p>
+      </section>
+    </>
+  );
+}

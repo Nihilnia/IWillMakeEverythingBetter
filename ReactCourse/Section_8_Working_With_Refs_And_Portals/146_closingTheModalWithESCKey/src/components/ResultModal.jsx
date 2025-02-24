63386368476db800ba9f109da221c02bf83405c3
Nihil <35506793+Nihilnia@users.noreply.github.com>
2025-02-24 23:56:34 +0300

    My way.

---
+++ ReactCourse/Section_8_Working_With_Refs_And_Portals/146_closingTheModalWithESCKey/src/components/ResultModal.jsx
@@ -0,0 +1,49 @@
+import { useImperativeHandle, useRef } from "react";
+
+export default function ResultModal({
+  ref,
+  targetTime,
+  remainingTime,
+  onReset,
+}) {
+  const dialog = useRef();
+
+  const isWin = remainingTime <= 0;
+  const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
+  const score = Math.round((1 - remainingTime / (targetTime * 1000)) * 100);
+
+  useImperativeHandle(ref, () => {
+    return {
+      openIt() {
+        dialog.current.showModal();
+      },
+    };
+  });
+
+  const aKey = useRef();
+
+  function handleKeyDown(e) {
+    if (e.key === "b") aKey.current.click();
+  }
+
+  return (
+    <dialog ref={dialog} className="result-modal">
+      {!isWin && <h2>Your score: {score}</h2>}
+      {isWin && <h2>You lost.</h2>}
+      <p>
+        The target time was <strong>{targetTime} seconds.</strong>
+      </p>
+      <p>
+        You stopped the timer with{" "}
+        <strong>{formattedRemainingTime} seconds left.</strong>
+      </p>
+      <form
+        method="dialog"
+        onSubmit={() => onReset()}
+        onKeyDown={handleKeyDown}
+      >
+        <button ref={aKey}>Close</button>
+      </form>
+    </dialog>
+  );
+}

ed0f49a6fe3a565abfe090c8ef9e61b1e6f76b00
Nihil <35506793+Nihilnia@users.noreply.github.com>
2025-02-28 21:05:38 +0300

    Okay, okay

--- ReactCourse/Section_9_Practice_Project_Project_Management_App/src/components/Input.jsx
+++ ReactCourse/Section_9_Practice_Project_Project_Management_App/src/components/Input.jsx
@@ -1,9 +1,12 @@
 export default function Input({ label, isTextarea, ...props }) {
-  const classes = "bg-stone-500";
+  const classes =
+    "w-full p-1 border-b-2 border-stone-300 rounded-sm bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600";
 
   return (
-    <p>
-      <label>{label}</label>
+    <p className="flex flex-col gap-1 my-4">
+      <label className="text-sm font-bold uppercase text-stone-500">
+        {label}
+      </label>
       {isTextarea ? (
         <textarea className={classes} {...props}></textarea>
       ) : (

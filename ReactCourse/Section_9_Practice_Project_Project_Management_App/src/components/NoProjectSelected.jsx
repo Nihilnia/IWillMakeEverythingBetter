5efd62772045a7fbe7efa962302c99d7f014bbf8
Nihil <35506793+Nihilnia@users.noreply.github.com>
2025-02-28 21:44:33 +0300

    Progress

---
+++ ReactCourse/Section_9_Practice_Project_Project_Management_App/src/components/NoProjectSelected.jsx
@@ -0,0 +1,23 @@
+import imgNoProject from "../assets/no-projects.png";
+import ButtonAdd from "./ButtonAdd";
+
+export default function NoProjectSelected() {
+  return (
+    <div className="mt-24 text-center w-2/3">
+      <img
+        src={imgNoProject}
+        alt="Select A Project"
+        className="w-16 h-16 object-contain mx-auto"
+      />
+      <h2 className="text-xl font-bold text-stone-500 my-4">
+        No Project Selected
+      </h2>
+      <p className="text-stone-400 mb-4">
+        Select a project or get started with a new one
+      </p>
+      <p>
+        <ButtonAdd>Create new project</ButtonAdd>
+      </p>
+    </div>
+  );
+}

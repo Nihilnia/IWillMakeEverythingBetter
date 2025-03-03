a37d2a58687344c30f47404693a998dd17c1b34e
Nihil <35506793+Nihilnia@users.noreply.github.com>
2025-03-04 02:11:08 +0300

    My way.
    Couldnt wait =) And btw forwarding refs and controlling inputs are the things that we need to work on.

--- ReactCourse/Section_9_Practice_Project_Project_Management_App/src/components/Tasks.jsx
+++ ReactCourse/Section_9_Practice_Project_Project_Management_App/src/components/Tasks.jsx
@@ -5,21 +5,31 @@ import Input from "./Input";
 export default function Tasks({ project, onAddNewTask }) {
   const newTask = useRef();
 
+  function handleNewTask() {
+    onAddNewTask(newTask.current.value);
+  }
+
+  const content =
+    project.tasks.length > 0 ? (
+      <ul>
+        {project.tasks.map((f, idx) => (
+          <li key={f.id}>
+            Task {idx + 1}- {f.title}
+          </li>
+        ))}
+      </ul>
+    ) : (
+      <p className="text-stone-800 mb-4">
+        This project does not have any tasks yet.
+      </p>
+    );
+
   return (
     <section>
       <h2 className="text-2xl font-bold text-stone-700 mb-4">Tasks</h2>
       <Input type="text" label="New Task:" ref={newTask} />
-      <ButtonAdd onClick={() => onAddNewTask(newTask.current.value)}>
-        Add
-      </ButtonAdd>
-      <p className="text-stone-800 mb-4">
-        This project does not have any tasks yet.
-      </p>
-      <ul>
-        {project.tasks.map((f) => (
-          <li key={f.id}>{f.title}</li>
-        ))}
-      </ul>
+      <ButtonAdd onClick={handleNewTask}>Add</ButtonAdd>
+      {content}
     </section>
   );
 }

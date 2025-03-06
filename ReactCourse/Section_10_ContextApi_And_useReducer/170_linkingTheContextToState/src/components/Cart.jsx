ac6640ff2658309094b8d4bd8763a2d595705c52
Nihil <35506793+Nihilnia@users.noreply.github.com>
2025-03-06 03:08:50 +0300

    I AM IN LOVE.

---
+++ ReactCourse/Section_10_ContextApi_And_useReducer/170_linkingTheContextToState/src/components/Cart.jsx
@@ -0,0 +1,50 @@
+import { useContext } from "react";
+import { use } from "react"; //Alternative and more flexible one, available /w React 19
+import { CartContext } from "../store/ShoppingCartContext";
+
+export default function Cart({ onUpdateItemQuantity }) {
+  const { items } = useContext(CartContext);
+
+  console.log("just want to try");
+  console.log(items); //just want to try : D
+
+  const totalPrice = items.reduce(
+    (acc, item) => acc + item.price * item.quantity,
+    0
+  );
+  const formattedTotalPrice = `$${totalPrice.toFixed(2)}`;
+
+  return (
+    <div id="cart">
+      {items.length === 0 && <p>No items in cart!</p>}
+      {items.length > 0 && (
+        <ul id="cart-items">
+          {items.map((item) => {
+            const formattedPrice = `$${item.price.toFixed(2)}`;
+
+            return (
+              <li key={item.id}>
+                <div>
+                  <span>{item.name}</span>
+                  <span> ({formattedPrice})</span>
+                </div>
+                <div className="cart-item-actions">
+                  <button onClick={() => onUpdateItemQuantity(item.id, -1)}>
+                    -
+                  </button>
+                  <span>{item.quantity}</span>
+                  <button onClick={() => onUpdateItemQuantity(item.id, 1)}>
+                    +
+                  </button>
+                </div>
+              </li>
+            );
+          })}
+        </ul>
+      )}
+      <p id="cart-total-price">
+        Cart Total: <strong>{formattedTotalPrice}</strong>
+      </p>
+    </div>
+  );
+}

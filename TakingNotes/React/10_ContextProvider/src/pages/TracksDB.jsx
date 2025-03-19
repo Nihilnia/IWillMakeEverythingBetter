add740cba1283e9f052f398ef43ac815003f4297
Nihil <35506793+Nihilnia@users.noreply.github.com>
2025-03-20 00:23:29 +0300

    Having everything in Context

--- TakingNotes/React/10_ContextProvider/src/pages/TracksDB.jsx
+++ TakingNotes/React/10_ContextProvider/src/pages/TracksDB.jsx
@@ -1,36 +1,10 @@
-import { useState } from "react";
+import TracksContextProvider from "../store/TracksContext";
 import TracksGrid from "../components/TracksGrid";
 
-import { TracksContext } from "../store/TracksContext";
-
 export default function TracksDB() {
-  const [allTracks, setAllTracks] = useState([
-    { id: 0, name: "Song_0" },
-    { id: 1, name: "Song_1" },
-    { id: 2, name: "Song_2" },
-    { id: 3, name: "Song_3" },
-    { id: 4, name: "Song_4" },
-    { id: 5, name: "Song_5" },
-    { id: 6, name: "Song_6" },
-    { id: 7, name: "Song_7" },
-    { id: 8, name: "Song_8" },
-    { id: 9, name: "Song_9" },
-  ]);
-
-  function handleNameChange(id, newName) {
-    setAllTracks((prev) => {
-      return prev.map((f) => (f.id === id ? { ...f, name: newName } : f));
-    });
-  }
-
-  const ctxValue = {
-    allTracks: allTracks,
-    handleChangeTrackName: handleNameChange,
-  };
-
   return (
-    <TracksContext.Provider value={ctxValue}>
+    <TracksContextProvider>
       <TracksGrid />
-    </TracksContext.Provider>
+    </TracksContextProvider>
   );
 }
--- TakingNotes/React/10_ContextProvider/src/store/TracksContext.jsx
+++ TakingNotes/React/10_ContextProvider/src/store/TracksContext.jsx
@@ -1,6 +1,36 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
 export const TracksContext = createContext({
   allTracks: [],
   handleChangeTrackName: () => {},
 });
+
+export default function TracksContextProvider({ children }) {
+  const [allTracks, setAllTracks] = useState([
+    { id: 0, name: "Song_0" },
+    { id: 1, name: "Song_1" },
+    { id: 2, name: "Song_2" },
+    { id: 3, name: "Song_3" },
+    { id: 4, name: "Song_4" },
+    { id: 5, name: "Song_5" },
+    { id: 6, name: "Song_6" },
+    { id: 7, name: "Song_7" },
+    { id: 8, name: "Song_8" },
+    { id: 9, name: "Song_9" },
+  ]);
+
+  function handleNameChange(id, newName) {
+    setAllTracks((prev) => {
+      return prev.map((f) => (f.id === id ? { ...f, name: newName } : f));
+    });
+  }
+
+  const ctxValue = {
+    allTracks: allTracks,
+    handleChangeTrackName: handleNameChange,
+  };
+
+  return (
+    <TracksContext.Provider value={ctxValue}>{children}</TracksContext.Provider>
+  );
+}

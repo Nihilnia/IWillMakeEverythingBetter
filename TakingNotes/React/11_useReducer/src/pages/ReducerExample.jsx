f598a20bb55c35e5b4e8ae20e69078072efa954f
Nihil <35506793+Nihilnia@users.noreply.github.com>
2025-03-20 15:34:29 +0300

    With reducer

--- TakingNotes/React/11_useReducer/src/pages/ReducerExample.jsx
+++ TakingNotes/React/11_useReducer/src/pages/ReducerExample.jsx
@@ -1,21 +1,36 @@
-import { useState } from "react";
+import { useReducer } from "react";
+
+function reducerExampleReducer(state, action) {
+  const { type } = action;
+
+  switch (type) {
+    case "PLUS":
+      return state + 1;
+    case "MINUS":
+      return state + -1;
+    case "RESET":
+      return 0;
+    default:
+      return 0;
+  }
+}
 
 export default function ReducerExample() {
-  const [count, setCount] = useState(0);
+  const [state, dispatch] = useReducer(reducerExampleReducer, 0);
 
   function handlePlus() {
-    setCount((prev) => prev + 1);
+    dispatch({ type: "PLUS" });
   }
   function handleMinus() {
-    setCount((prev) => prev - 1);
+    dispatch({ type: "MINUS" });
   }
   function handleReset() {
-    setCount(0);
+    dispatch({ type: "RESET" });
   }
 
   return (
     <section>
-      <h2>Count: {count}</h2>
+      <h2>Count: {state}</h2>
       <button onClick={handlePlus}>Plus 1</button>
       <button onClick={handleMinus}>Minus 1</button>
       <button onClick={handleReset}>Reset</button>

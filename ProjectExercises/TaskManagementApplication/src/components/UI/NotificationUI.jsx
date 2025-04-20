5a677d5a03d583b37a6bbc0bdeb1db6275db5586
Nihil <35506793+Nihilnia@users.noreply.github.com>
2025-04-20 05:00:13 +0300

    Hello, again.

--- ProjectExercises/TaskManagementApplication/src/components/UI/NotificationUI.jsx
+++
@@ -1,8 +0,0 @@
-import { createPortal } from "react-dom";
-
-export default function NotificationUI({ message }) {
-  return createPortal(
-    <section className="fixed bottom-2 left-2">{message}</section>,
-    document.getElementById("notification")
-  );
-}

cf32df97b76849dcdc6b66c627a17b82bcac840d
Nihil <35506793+Nihilnia@users.noreply.github.com>
2025-05-27 13:08:13 +0300

    Beautiful everything

--- ReactCourse/Section_15_Sending_Http_Requests/231_preparingTheAppForDataFetching/src/components/AvailablePlaces.jsx
+++ ReactCourse/Section_15_Sending_Http_Requests/231_preparingTheAppForDataFetching/src/components/AvailablePlaces.jsx
@@ -1,12 +1,18 @@
-import Places from './Places.jsx';
+import { useState } from "react";
+import Places from "./Places.jsx";
 
 export default function AvailablePlaces({ onSelectPlace }) {
-  return (
-    <Places
-      title="Available Places"
-      places={[]}
-      fallbackText="No places available."
-      onSelectPlace={onSelectPlace}
-    />
-  );
+	const [availablePlaces, setAvailablePlaces] = useState([]);
+
+	//We set the initial value of the state as an empty array
+	//When component starts to get rendered we will fetch the data from the db
+
+	return (
+		<Places
+			title="Available Places"
+			places={[]}
+			fallbackText="No places available."
+			onSelectPlace={onSelectPlace}
+		/>
+	);
 }

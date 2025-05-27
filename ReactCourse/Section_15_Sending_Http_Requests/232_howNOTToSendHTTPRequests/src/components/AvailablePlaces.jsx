b4a381b09aec876a1b8dff39d84328483a7f18e5
Nihil <35506793+Nihilnia@users.noreply.github.com>
2025-05-27 13:53:40 +0300

    Hhahahhaha, How to NOT

--- ReactCourse/Section_15_Sending_Http_Requests/232_howNOTToSendHTTPRequests/src/components/AvailablePlaces.jsx
+++ ReactCourse/Section_15_Sending_Http_Requests/232_howNOTToSendHTTPRequests/src/components/AvailablePlaces.jsx
@@ -1,11 +1,23 @@
 import { useState } from "react";
 import Places from "./Places.jsx";
+import { useEffect } from "react";
 
 export default function AvailablePlaces({ onSelectPlace }) {
 	const [availablePlaces, setAvailablePlaces] = useState([]);
 
 	//We set the initial value of the state as an empty array
 	//When component starts to get rendered we will fetch the data from the db
+
+	fetch("http://localhost:3000/places")
+		.then((response) => {
+			return response.json();
+		})
+		.then((respData) => {
+			setAvailablePlaces(respData.places);
+		});
+
+	console.log("availablePlaces");
+	console.log(availablePlaces);
 
 	return (
 		<Places

5d2217c86bb8abcd3a59ec5a83957a3c61c762dd
Nihil <35506793+Nihilnia@users.noreply.github.com>
2025-05-28 17:18:56 +0300

    Start

---
+++ ReactCourse/Section_15_Sending_Http_Requests/239_extractingCodeNDImprovingCodeStructure/src/components/AvailablePlaces.jsx
@@ -0,0 +1,54 @@
+import { useState } from "react";
+import Places from "./Places.jsx";
+import { useEffect } from "react";
+import { sortPlacesByDistance } from "../loc.js";
+import Error from "./Error.jsx";
+
+export default function AvailablePlaces({ onSelectPlace }) {
+	const [availablePlaces, setAvailablePlaces] = useState([]);
+	const [isLoading, setLoading] = useState(true);
+	const [isError, setIsError] = useState(false);
+
+	useEffect(() => {
+		async function fetchPlaces() {
+			try {
+				const resp = await fetch("http://localhost:3000/places");
+
+				if (!resp.ok) {
+					throw new Error("Something went wrong..");
+				}
+
+				const respData = await resp.json();
+
+				navigator.geolocation.getCurrentPosition((pos) => {
+					const sortedPlaces = sortPlacesByDistance(
+						respData.places,
+						pos.coords.latitude,
+						pos.coords.longitude,
+					);
+
+					setAvailablePlaces(sortedPlaces);
+					setLoading(false);
+				});
+			} catch (err) {
+				setIsError(err.message || "Couldn' t fetch places, try again later.");
+			}
+		}
+
+		fetchPlaces();
+	}, []);
+
+	if (isError)
+		return <Error title="Something went wrong.." message={isError} />;
+
+	return (
+		<Places
+			title="Available Places"
+			places={availablePlaces}
+			isLoading={isLoading}
+			loadingText={"Loading.."}
+			fallbackText="No places available."
+			onSelectPlace={onSelectPlace}
+		/>
+	);
+}

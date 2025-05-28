85c6bf663ead59576fd2e34e29d6793437122e32
Nihil <35506793+Nihilnia@users.noreply.github.com>
2025-05-28 16:28:37 +0300

    We good

--- ReactCourse/Section_15_Sending_Http_Requests/237_handlingHTTPErrors/src/components/AvailablePlaces.jsx
+++ ReactCourse/Section_15_Sending_Http_Requests/237_handlingHTTPErrors/src/components/AvailablePlaces.jsx
@@ -2,27 +2,44 @@ import { useState } from "react";
 import Places from "./Places.jsx";
 import { useEffect } from "react";
 
+import Error from "./Error.jsx";
+
 export default function AvailablePlaces({ onSelectPlace }) {
 	const [availablePlaces, setAvailablePlaces] = useState([]);
-	const [isLoading, setIsLoading] = useState(true);
+	const [isLoading, setLoading] = useState(true);
+	const [isError, setIsError] = useState(false);
 
 	useEffect(() => {
 		async function fetchPlaces() {
-			const response = await fetch("http://localhost:3000/places");
-			const respData = await response.json();
-			setAvailablePlaces(respData.places);
-			setIsLoading(false);
+			try {
+				const resp = await fetch("http://localhost:3000/placess");
+
+				if (!resp.ok) {
+					throw new Error("Something went wrong..");
+				}
+
+				const respData = await resp.json();
+
+				setAvailablePlaces(respData.places);
+			} catch (err) {
+				setIsError(err.message || "Couldn' t fetch places, try again later.");
+			}
+
+			setLoading(false);
 		}
 
 		fetchPlaces();
 	}, []);
+
+	if (isError)
+		return <Error title="Something went wrong.." message={isError} />;
 
 	return (
 		<Places
 			title="Available Places"
 			places={availablePlaces}
 			isLoading={isLoading}
-			loadingText={"Fetching place data.."}
+			loadingText={"Loading.."}
 			fallbackText="No places available."
 			onSelectPlace={onSelectPlace}
 		/>

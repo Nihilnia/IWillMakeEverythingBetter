1ec25d3ca7fcff283fb6b2fcef972e243a5cc2f6
Nihil <35506793+Nihilnia@users.noreply.github.com>
2025-05-28 13:58:28 +0300

    Okay

--- ReactCourse/Section_15_Sending_Http_Requests/235_handlingLoadingStates/src/components/AvailablePlaces.jsx
+++ ReactCourse/Section_15_Sending_Http_Requests/235_handlingLoadingStates/src/components/AvailablePlaces.jsx
@@ -4,27 +4,25 @@ import { useEffect } from "react";
 
 export default function AvailablePlaces({ onSelectPlace }) {
 	const [availablePlaces, setAvailablePlaces] = useState([]);
-
-	//We set the initial value of the state as an empty array
-	//When component starts to get rendered we will fetch the data from the db
+	const [isLoading, setIsLoading] = useState(true);
 
 	useEffect(() => {
 		async function fetchPlaces() {
 			const response = await fetch("http://localhost:3000/places");
 			const respData = await response.json();
 			setAvailablePlaces(respData.places);
+			setIsLoading(false);
 		}
 
 		fetchPlaces();
 	}, []);
 
-	console.log("availablePlaces");
-	console.log(availablePlaces);
-
 	return (
 		<Places
 			title="Available Places"
 			places={availablePlaces}
+			isLoading={isLoading}
+			loadingText={"Fetching place data.."}
 			fallbackText="No places available."
 			onSelectPlace={onSelectPlace}
 		/>

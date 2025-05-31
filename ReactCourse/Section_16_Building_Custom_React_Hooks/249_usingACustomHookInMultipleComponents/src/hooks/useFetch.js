6cef494640b20505324207c27dd3dbd26efebc00
Nihil <35506793+Nihilnia@users.noreply.github.com>
2025-05-31 14:31:41 +0300

    Start

---
+++ ReactCourse/Section_16_Building_Custom_React_Hooks/249_usingACustomHookInMultipleComponents/src/hooks/useFetch.js
@@ -0,0 +1,31 @@
+import { useState, useEffect } from "react";
+
+export function useFetch(fetchFn, url, initialVal) {
+	const [isFetching, setIsFetching] = useState();
+	const [error, setError] = useState();
+	const [fetchedData, setFetchedData] = useState(initialVal);
+
+	useEffect(() => {
+		async function fetchData() {
+			setIsFetching(true);
+			try {
+				const data = await fetchFn(url);
+
+				setFetchedData(data);
+			} catch (error) {
+				setError({ message: error.message || "Failed to fetch data.." });
+			}
+
+			setIsFetching(false);
+		}
+
+		fetchData(fetchFn);
+	}, []);
+
+	return {
+		isFetching,
+		error,
+		fetchedData,
+		setFetchedData,
+	};
+}

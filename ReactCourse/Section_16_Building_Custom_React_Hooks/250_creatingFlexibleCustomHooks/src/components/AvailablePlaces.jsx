57f17635caea1c3f9c7a6ca40317427891929d80
Nihil <35506793+Nihilnia@users.noreply.github.com>
2025-06-01 14:45:12 +0300

    Okay
    I mean.. What if user doesnt allow location access

--- ReactCourse/Section_16_Building_Custom_React_Hooks/250_creatingFlexibleCustomHooks/src/components/AvailablePlaces.jsx
+++ ReactCourse/Section_16_Building_Custom_React_Hooks/250_creatingFlexibleCustomHooks/src/components/AvailablePlaces.jsx
@@ -2,26 +2,30 @@ import Places from "./Places.jsx";
 import Error from "./Error.jsx";
 import { useFetch } from "../hooks/useFetch.js";
 import { fetchAvailablePlaces } from "../http.js";
-import { useEffect } from "react";
 import { sortPlacesByDistance } from "../loc.js";
+
+async function getSortedPlaces() {
+	const places = await fetchAvailablePlaces();
+
+	return new Promise((resolve) => {
+		navigator.geolocation.getCurrentPosition((pos) => {
+			const sortedPlaces = sortPlacesByDistance(
+				places,
+				pos.coords.latitude,
+				pos.coords.longitude,
+			);
+
+			resolve(sortedPlaces);
+		});
+	});
+}
 
 export default function AvailablePlaces({ onSelectPlace }) {
 	const {
 		isFetching,
 		error,
 		fetchedData: availablePlaces,
-		setFetchedData: setAvailablePlaces,
-	} = useFetch(fetchAvailablePlaces, "http://localhost:3000/places", []);
-
-	// navigator.geolocation.getCurrentPosition((pos) => {
-	// 	setAvailablePlaces(() => {
-	// 		return sortPlacesByDistance(
-	// 			availablePlaces,
-	// 			pos.coords.latitude,
-	// 			pos.coords.longitude,
-	// 		);
-	// 	});
-	// });
+	} = useFetch(getSortedPlaces, "http://localhost:3000/places", []);
 
 	if (error) {
 		return <Error title="An error occurred!" message={error.message} />;

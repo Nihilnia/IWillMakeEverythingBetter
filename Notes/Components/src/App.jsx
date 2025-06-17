b890aab8b7abcbeabb61dbd272363014ba735353
Nihil <35506793+Nihilnia@users.noreply.github.com>
2025-06-17 20:58:23 +0300

    Braindance Calibration

---
+++ Notes/Components/src/App.jsx
@@ -0,0 +1,5 @@
+import Gloria from "./components/Gloria";
+
+export default function App() {
+  return <Gloria />;
+}
---
+++ Notes/Components/src/components/Gloria.jsx
@@ -0,0 +1,5 @@
+export default function Gloria() {
+  const name = "Gloria";
+
+  return <h2>Hello, {name}</h2>;
+}

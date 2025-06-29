ccdca3e7fa5753f4288c06f0a739b6b8184a0866
Nihil <35506793+Nihilnia@users.noreply.github.com>
2025-06-29 22:38:45 +0300

    Start

--- ReactCourse/Section_18_Handling_Forms_Via_Form_Actions/273_aDemoApp_Introduction/src/components/NewOpinion.jsx
+++ ReactCourse/Section_18_Handling_Forms_Via_Form_Actions/273_aDemoApp_Introduction/src/components/NewOpinion.jsx
@@ -1,63 +1,23 @@
-import { useActionState, useContext } from "react";
-import submitOpinion from "../util/submitOpinion";
-import { OpinionsContext } from "../store/opinions-context";
-
 export function NewOpinion() {
-  const { addOpinion } = useContext(OpinionsContext);
-
-  const [formState, formAction, pending] = useActionState(submitOpinion, { errors: null });
-
-  if (formState.isFormSent) {
-    const { username, title, body } = formState.enteredValues;
-
-    const newOpinion = {
-      id: Math.random(),
-      userName: username,
-      title: title,
-      body: body,
-      votes: 0,
-    };
-
-    addOpinion(newOpinion);
-  }
-
   return (
     <div id="new-opinion">
       <h2>Share your opinion!</h2>
-      <form action={formAction}>
+      <form>
         <div className="control-row">
           <p className="control">
             <label htmlFor="userName">Your Name</label>
-            <input
-              type="text"
-              id="userName"
-              name="userName"
-              defaultValue={formState.enteredValues?.username}
-            />
+            <input type="text" id="userName" name="userName" />
           </p>
 
           <p className="control">
             <label htmlFor="title">Title</label>
-            <input
-              type="text"
-              id="title"
-              name="title"
-              defaultValue={formState.enteredValues?.title}
-            />
+            <input type="text" id="title" name="title" />
           </p>
         </div>
         <p className="control">
           <label htmlFor="body">Your Opinion</label>
-          <textarea id="body" name="body" rows={5} defaultValue={formState.enteredValues?.body} />
+          <textarea id="body" name="body" rows={5}></textarea>
         </p>
-
-        {formState.errors && (
-          <ul className="errors">
-            {formState.errors.map((err) => {
-              return <li key={err}>{err}</li>;
-            })}
-          </ul>
-        )}
 
         <p className="actions">
           <button type="submit">Submit</button>
--- ReactCourse/Section_18_Handling_Forms_Via_Form_Actions/273_aDemoApp_Introduction/src/util/validations.js
+++
@@ -1,7 +0,0 @@
-export function isLongEnough(minLength, value) {
-  if (value.length >= minLength) {
-    return true;
-  }
-
-  return false;
-}

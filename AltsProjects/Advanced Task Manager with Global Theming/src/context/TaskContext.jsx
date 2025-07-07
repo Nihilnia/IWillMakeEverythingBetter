9f36cba486f6547db5adfd02eb4cc2861c3c25b7
Nihil <35506793+Nihilnia@users.noreply.github.com>
2025-07-07 06:18:26 +0300

    Zero

---
+++ AltsProjects/Advanced Task Manager with Global Theming/src/context/TaskContext.jsx
@@ -0,0 +1,87 @@
+import { createContext, useReducer } from "react";
+
+export const TaskContext = createContext({
+  allTasks: [],
+  addTask: () => {},
+  removeTask: () => {},
+  editTask: () => {},
+});
+
+function TaskCRUDReducer(state, action) {
+  const { type, payload } = action;
+
+  switch (type) {
+    case "ADD_TASK": {
+      const newTask = { ...payload.newTask, id: Math.random, isCompleted: false };
+      return [...state, newTask];
+    }
+
+    case "REMOVE_TASK": {
+      const { id } = payload;
+      const shallowCopy = state.filter((task) => {
+        return task.id !== id;
+      });
+      return shallowCopy;
+    }
+
+    case "EDIT_TASK": {
+      const { id, newDetails } = payload;
+
+      const foundTask = state.filter((task) => {
+        return task.id === id;
+      });
+
+      const taskWithNewDetails = {
+        ...foundTask,
+        ...newDetails,
+      };
+
+      const shallowCopy = state.map((task) => {
+        return task.id === id ? taskWithNewDetails : task;
+      });
+
+      return shallowCopy;
+    }
+  }
+}
+
+export default function TaskContextProvider({ children }) {
+  const [allTasks, dispatch] = useReducer(TaskCRUDReducer, []);
+
+  function addTask(newTask) {
+    dispatch({
+      type: "ADD_TASK",
+      payload: {
+        newTask,
+      },
+    });
+  }
+
+  function editTask(id, newDetails) {
+    dispatch({
+      type: "EDIT_TASK",
+      payload: {
+        id,
+        newDetails,
+      },
+    });
+  }
+
+  function removeTask(id) {
+    dispatch({
+      type: "REMOVE_TASK",
+      payload: {
+        id,
+      },
+    });
+  }
+
+  const ctxValues = {
+    allTasks,
+    addTask,
+    editTask,
+    removeTask,
+  };
+
+  return <TaskContext.Provider value={ctxValues}>{children}</TaskContext.Provider>;
+}

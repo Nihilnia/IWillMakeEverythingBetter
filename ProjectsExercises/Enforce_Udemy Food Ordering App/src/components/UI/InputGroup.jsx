950ea9f0db20d1182225d97ab459e89ae59a61f0
Nihil <35506793+Nihilnia@users.noreply.github.com>
2025-07-31 14:03:06 +0300

    Basic validation

--- ProjectsExercises/Enforce_Udemy Food Ordering App/src/components/UI/InputGroup.jsx
+++ ProjectsExercises/Enforce_Udemy Food Ordering App/src/components/UI/InputGroup.jsx
@@ -2,7 +2,7 @@ export default function InputGroup({ labelTitle, labelFor, ...props }) {
   return (
     <div className="control">
       <label htmlFor={labelFor}>{labelTitle}</label>
-      <input type="text" id={labelFor} name={labelFor} {...props} />
+      <input type="text" id={labelFor} name={labelFor} required {...props} />
     </div>
   );
 }
